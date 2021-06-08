bf2f630d6f910831a7fd854c3f972847b80ad26b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-06-08 22:36:26 +0000

    Deploy philipbeber/packing-list-demo to github.com/philipbeber/packing-list-demo.git:gh-pages

---
+++ demo/precache-manifest.db09ecc4c91c2d13d062c94a480b8857.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da2509d9dfa20fe84cab52a4a99fb571",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b4651ec9b1f96db35cc5",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7bcb2f579e2870f60c67",
+    "url": "./static/js/2.9fad3f98.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "./static/js/2.9fad3f98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4651ec9b1f96db35cc5",
+    "url": "./static/js/main.241ceee6.chunk.js"
+  },
+  {
+    "revision": "2830f5541896f051e819",
+    "url": "./static/js/runtime-main.48a42413.js"
+  }
+]);
